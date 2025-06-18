48b3f7e21475eab5b1317ac8a7603518604b8a1f
SushmithaM13 <sushmithagoud13@gmail.com>
2025-06-18 10:53:30 +0530

    implemented Appointment section

---
+++ Login_Registration/src/pages/Resouces/page1.jsx
@@ -0,0 +1,9 @@
+
+const Page1=()=>{
+    return(
+        <>
+        <h1>welcome</h1>
+        </>
+    )
+}
+export default Page1;
--- Login_Registration/src/routes/RoutingAll.jsx
+++ Login_Registration/src/routes/RoutingAll.jsx
@@ -4,6 +4,7 @@ import Login from "../components/Auth/login";
 import SignUp from "../components/Auth/signUp";
 import Home from "../components/home";
 import Appointments from "../pages/Appointments/appointments";
+import Page1 from "../pages/Resouces/page1";
 
 
 export const RoutingAll = () => {
@@ -13,34 +14,9 @@ export const RoutingAll = () => {
       <Route path="/signUp" element={<SignUp/>} />
       <Route path="/" element={<PrivateRoute><Home /></PrivateRoute>}/>
       <Route path="/appointments" element={<Appointments/>} />
+      <Route path="/page1" element={<Page1/>} />
     </Routes>
   );
 };
 
 export default RoutingAll;
-
-
-
-
-
-
-// import { Routes, Route } from "react-router-dom";
-// import Login from "../pages/login";
-// import SignUp from "../pages/signUp";
-// import Page from "../pages/page";
-// import Home from "../pages/home";
-
-// export const RoutingAll=()=>{
-//     return(
-//         <>
-//         <Routes>
-//             <Route path="/" element={<Home/>}/>
-//             <Route path="/login" element={<Login/>}/>
-//             <Route path="/signUp" element={<SignUp/>}/>
-//              <Route path="/page1" element={<Page/>} />
-//         </Routes>
-//         </>
-//     )
-// }
-
-// export default RoutingAll;
